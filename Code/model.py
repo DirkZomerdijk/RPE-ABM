2e174d47f6854b5d4f3aad781d720140c5d7cb66
mannusschomaker <31698450+mannusschomaker@users.noreply.github.com>
2019-01-11 14:35:49 +0100

    network installed

--- Code/model.py
+++ Code/model.py
@@ -1,12 +1,16 @@
 import networkx as nx
 from mesa import Agent, Model
 from mesa.time import RandomActivation
+from agents import *
 
-class NetworkModel(Model):
+class Network(Model):
     def __init__(self, N):
         self.num_agents = N
-
-        G = nx.watts_strogatz_graph(20, 2, 0, seed=None)
+        self.G = nx.watts_strogatz_graph(20, 2, 0, seed=None)
         print(G.nodes)
 
-test = NetworkModel(20)
+    def fill_network(self, node):
+        nx.set_node_attributes(self.G, [node.get_rand_expectation()]*self.num_agents, 'expectation')
+
+
+test = Network(20)
