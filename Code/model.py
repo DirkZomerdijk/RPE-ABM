5cfdaca781635d9ad062dc6ee692a4f840b9d540
mannusschomaker <31698450+mannusschomaker@users.noreply.github.com>
2019-01-13 15:08:40 +0100

    new commit

--- Code/model.py
+++ Code/model.py
@@ -7,9 +7,15 @@ class Network(Model):
     def __init__(self, N):
         self.num_agents = N
         self.G = nx.watts_strogatz_graph(20, 2, 0, seed=None)
-        print(G.nodes)
+        print(self.G.nodes)
 
-    def fill_network(self, node):
+        self.num_agents = N
+        self.num_nodes = num_nodes if num_nodes >= self.num_agents else self.num_agents
+        self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=0.5)
+        self.grid = NetworkGrid(self.G)
+        self.schedule = RandomActivation(self)
+
+    def fill_network(self):
         nx.set_node_attributes(self.G, [node.get_rand_expectation()]*self.num_agents, 'expectation')
 
 
