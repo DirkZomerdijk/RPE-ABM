e73fa39bbee5c30d5086bbeb1f5f062e29bdd36d
mannusschomaker <31698450+mannusschomaker@users.noreply.github.com>
2019-01-14 13:32:24 +0100

    utility fix

--- Code/utility.py
+++ Code/utility.py
@@ -12,9 +12,6 @@ def get_rand_expectation():
     mean = 5.5
     stdev = 5.5/5.5   # 99.73% chance the sample will fall in your desired range
 
-<<<<<<< HEAD
-    return float("%.2f" % random.gauss(mean, stdev))
-=======
     return float("%.2f" % random.gauss(mean, stdev))
 
 def get_type():
@@ -28,4 +25,3 @@ def get_payoff_matrix(reputationA,reputationB,convincing_powerA,convincing_power
  #             [[convincing_powerA, reputationB], [convincing_powerA, convincing_powerB]] ] )
     return np.array([[reputationA, convincing_powerA], [reputationB,convincing_powerB]])
 
->>>>>>> 479059b9b13cde3ae3645a8217bd82b53f138ece
