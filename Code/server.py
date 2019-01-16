1d64cb30a5c99405a315c74ef6fd396b6a58fe9d
rusty <bendickens@zoho.com>
2019-01-16 14:26:13 +0100

    Added beta component slider for Watts-Strogatz network

--- Code/server.py
+++ Code/server.py
@@ -40,11 +40,13 @@ chart = ChartModule([{"Label": "expectations",
 agents_slider = UserSettableParameter('slider', "Number of Agents", 10, 2, 800, 1)
 neighbors_slider = UserSettableParameter('slider', "Number of Neighbors", 3, 2, 10, 1)
 network_slider = UserSettableParameter('slider', "Network Type", 1,1,2,1)
+beta_slider = UserSettableParameter('slider', "Beta Component (Only for Watts-Strogatz [1])", 0.5, 0,1,0.01)
 
 model_params = {
     "N": agents_slider,
     "no_of_neighbors": neighbors_slider,
     "network_type": network_slider, 
+    "beta_component": beta_slider,
 }  
 
 
