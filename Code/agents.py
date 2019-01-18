617a8b5946dff15acfd9ae5b44c9b4e66c375d45
DirkZomerdijk <dirkzomerdijk@gmail.com>
2019-01-18 13:07:02 +0100

    nothing

--- Code/agents.py
+++ Code/agents.py
@@ -32,11 +32,14 @@ class agent(Agent):
             if random.uniform(0,1) < probability_rate_A:
                 
                 self.opinion = 0
-                self.preference = self.preference + (K* sum([x - self.preference for x in A_preference]))%1
+                # print(K*(sum([(x - self.preference) for x in A_preference])))
+                self.preference = self.preference + K*(sum([(x - self.preference)%1 for x in A_preference]))
                 # self.preference = probability_rate_A
             else:
                 self.opinion = 1
-                self.preference = self.preference + (K* sum([x - self.preference for x in B_preference]))%1
+                # print(K*(sum([(x - self.preference) for x in A_preference])))
+
+                self.preference = self.preference + K*(sum([(x - self.preference)%1 for x in B_preference]))
                 # self.preference = probability_rate_B
 
     def choose_neighbors(self, neighbors):
